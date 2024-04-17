aef291b68691165370ecfc554584bccac29fba6a
Beti M <betelhemmessele@gmail.com>
2024-04-17 16:56:09 +0200

    reseau pages tabs added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+
+const tabHeadingAll = document.querySelectorAll(".tabs-container .tab-heading");
+const tabContentAll = document.querySelectorAll(".tabs-container .tab-content");
+
+const removeAllActive = () => {
+  tabContentAll.forEach((c) => {
+    c.classList.remove("active");
+  });
+
+  tabHeadingAll.forEach((h) => {
+    h.classList.remove("active");
+  });
+};
+
+tabHeadingAll.forEach((h, i) => {
+  h.addEventListener("click", () => {
+    removeAllActive();
+    tabContentAll[i].classList.add("active");
+    h.classList.add("active");
+  });
+});
+
+
 let items = document.querySelectorAll('.slider .list .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
