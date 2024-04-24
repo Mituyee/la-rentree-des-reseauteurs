25f1e6809b52ad5ab880dfb03936e6e1b6354ab2
Beti M <betelhemmessele@gmail.com>
2024-04-24 14:17:09 +0200

    Gallery page added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // tabs
 
 const tabHeadingAll = document.querySelectorAll(".tabs-container .tab-heading");
@@ -21,56 +20,52 @@ tabHeadingAll.forEach((h, i) => {
     h.classList.add("active");
   });
 });
-// end of tabs 
-
+// end of tabs
 
 // slide show
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
+let items = document.querySelectorAll(".slider .list .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
 
 // config param
 let countItem = items.length;
 let itemActive = 0;
 // event next click
-next.onclick = function(){
-    itemActive = itemActive + 1;
-    if(itemActive >= countItem){
-        itemActive = 0;
-    }
-    showSlider();
-}
+next.onclick = function () {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+};
 //event prev click
-prev.onclick = function(){
-    itemActive = itemActive - 1;
-    if(itemActive < 0){
-        itemActive = countItem - 1;
-    }
-    showSlider();
-}
+prev.onclick = function () {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+};
 // auto run slider
 let refreshInterval = setInterval(() => {
+  next.click();
+}, 5000);
+function showSlider() {
+  // remove item active old
+  let itemActiveOld = document.querySelector(".slider .list .item.active");
+  itemActiveOld.classList.remove("active");
+
+  // active new item
+  items[itemActive].classList.add("active");
+
+  // clear auto time run slider
+  clearInterval(refreshInterval);
+  refreshInterval = setInterval(() => {
     next.click();
-}, 5000)
-function showSlider(){
-    // remove item active old
-    let itemActiveOld = document.querySelector('.slider .list .item.active');
-    itemActiveOld.classList.remove('active');
-    
-
-    // active new item
-    items[itemActive].classList.add('active');
-
-
-    // clear auto time run slider
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(() => {
-        next.click();
-    }, 5000)
+  }, 5000);
 }
 
 // end of slide show
-
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
@@ -82,4 +77,20 @@ function myFunction() {
   }
 }
 
-// end of mobile navigation menu 
+// end of mobile navigation menu
+
+// Gallery page
+
+// Open the modal with the clicked image
+function openModal(imageUrl) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImg");
+  modal.style.display = "block";
+  modalImg.src = imageUrl;
+}
+
+// Close the modal
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
